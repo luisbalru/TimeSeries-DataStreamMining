c463227d8513cf68c770f7073d6c1d62075e35fe
luisbalru <balderasruizluis@gmail.com>
2020-04-10 18:55:09 +0200

    empezando mensual

--- TimeSeries/TrabajoAutonomo/Estacion2870_mensual_77145416.R
+++ TimeSeries/TrabajoAutonomo/Estacion2870_mensual_77145416.R
@@ -4,6 +4,8 @@
 
 # setwd("~/Documentos/Master/TimeSeries-DataStreamMining/TimeSeries/TrabajoAutonomo")
 
+NPred = 2 # Valores a predecir
+NTest = 12 # Valores para test
 
 library(tseries)
 
@@ -11,3 +13,11 @@ serie = scan('./data/Estacion2870_mensual.txt')
 
 serie.ts = ts(serie,frequency = 12)
 plot(decompose(serie.ts))
+
+serieTr = serie.ts[1:(length(serie.ts)-NTest)]
+tiempoTr = 1:length(serieTr)
+serieTs = serie.ts[(length(serie.ts)-NTest+1):length(serie)]
+tiempoTs = (tiempoTr[length(tiempoTr)]+1):(tiempoTr[length(tiempoTr)]+NTest)
+
+plot.ts(serieTr, xlim=c(1,tiempoTs[length(tiempoTs)]))
+lines(tiempoTs,serieTs,col='red')
