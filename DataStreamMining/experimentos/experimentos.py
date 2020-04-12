7c126484362e5adf5c1f8d025d4645384df509b8
luisbalru <balderasruizluis@gmail.com>
2020-04-12 19:30:05 +0200

    vamos con el punto 2

--- DataStreamMining/experimentos/experimentos.py
+++ DataStreamMining/experimentos/experimentos.py
@@ -9,12 +9,39 @@ import pandas as pd
 import numpy as np
 from scipy.stats import shapiro
 
+def comparaAlgoritmos(muestra1,muestra2,nombre1,nombre2):
+    print("Shapiro para " + nombre1)
+    shap1 = shapiro(muestra1)
+    print(shap1)
+    print("Shapiro para " + nombre2)
+    shap2 = shapiro(muestra2)
+    print(shap2)
+    from scipy import stats
+    if shap1[1] > 0.05 and shap2[1] > 0.05:
+        print(stats.ttest_ind(muestra1,muestra2))
+        print("Promedio de acierto de " + nombre1 + ": " + str(np.mean(muestra1)))
+        print("Promedio de acierto de " + nombre2 + ": " + str(np.mean(muestra2)))
+    else:
+        print(stats.mannwhitneyu(muestra1,muestra2))
+        if shap1[1] > 0.05:
+            print("Promedio de acierto de " + nombre1 + ": " + str(np.mean(muestra1)))
+        else:
+            print("Promedio de acierto de " + nombre1 + ": " + str(np.median(muestra1)))
+        if shap2[1] > 0.05:
+            print("Promedio de acierto de " + nombre2 + ": " + str(np.mean(muestra2)))
+        else:
+            print("Promedio de acierto de " + nombre2 + ": " + str(np.median(muestra2)))
+
+
 def EjercicioOffline(adaptativo=False):
     print("Entrenamiento estacionario")
     print("Clasificador Hoeffding adaptativo: ", adaptativo)
     for i in range(4,34):
         if not adaptativo:
-            query = 'java -cp moa.jar -javaagent:sizeofag-1.0.4.jar moa.DoTask "EvaluateModel -m (LearnModel -l trees.HoeffdingTree -s (generators.WaveformGenerator -i '+str(i)+') -m 1000000) -s (generators.WaveformGenerator -i 2) -i 1000000" > offline/HNA/hna'+str(i)+'.csv'
+            query = 'java -cp moa.jar -javaagent:sizeofag-1.0.4.jar moa.DoTask \
+                    "EvaluateModel -m (LearnModel -l trees.HoeffdingTree -s \
+                    (generators.WaveformGenerator -i '+str(i)+') -m 1000000) \
+                    -s (generators.WaveformGenerator -i 2) -i 1000000" > offline/HNA/hna'+str(i)+'.csv'
             os.system(query)
         else:
             os.system('java -cp moa.jar -javaagent:sizeofag-1.0.4.jar moa.DoTask \
@@ -39,6 +66,7 @@ def EvaluacionEjercicioOffline(dir1='offline/HNA', dir2 = 'offline/HA'):
         ha_kap.append(data.iloc[-1,3:4][0])
     resultado = pd.DataFrame({"i":np.arange(4,34).tolist(),'Accuracy HNA':hna_acc,'Kappa HNA':hna_kap, 'Accuracy HA':ha_acc, 'Kappa HA': ha_kap})
     resultado.to_csv('ejercicio_offline.csv',index=False)
+    comparaAlgoritmos(resultado['Accuracy HNA'], resultado['Accuracy HA'], 'Accuracy HNA', 'Accuracy HA')
     return(resultado)
 
 
